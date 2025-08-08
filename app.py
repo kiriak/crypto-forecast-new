316237b9fc80f96b04b321d84be034b28c8cf99d
Kyriakos Noves <kn_macpro-13zoll@MacBook-Air-von-Kyriakos.local>
2025-08-08 19:13:07 +0200

    Fixed IndentationError in app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-    import os
-    from web_dashboard import app
-    
-    if __name__ == '__main__':
-        PORT = os.environ.get("PORT", 5000)
-        app.run(host='0.0.0.0', port=int(PORT))
-    
+import os
+from web_dashboard import app
+
+if __name__ == '__main__':
+    PORT = os.environ.get("PORT", 5000)
+    app.run(host='0.0.0.0', port=int(PORT))
